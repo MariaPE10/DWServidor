883ffbd4e9accf0cb5b40a0d032eb7b415f731be
alumno <alumno@PC-XX.daw05.com>
2019-11-20 10:31:35 +0100

    Primer commit

---
+++ demo/src/test/java/org/iesvelazquez/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesvelazquez.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
